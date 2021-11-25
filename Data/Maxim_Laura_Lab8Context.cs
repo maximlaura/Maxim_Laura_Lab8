ff67355dd6a678d04d1c3bd850a2f179da6e01c4
Laura Daniela Maxim <Maxim.Da.Laura@student.utcluj.ro>
2021-11-25 21:39:13 +0200

    Add project files.

---
+++ Data/Maxim_Laura_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Maxim_Laura_Lab8.Models;
+
+namespace Maxim_Laura_Lab8.Data
+{
+    public class Maxim_Laura_Lab8Context : DbContext
+    {
+        public Maxim_Laura_Lab8Context (DbContextOptions<Maxim_Laura_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Maxim_Laura_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Maxim_Laura_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Maxim_Laura_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Maxim_Laura_Lab8.Data;
+using Maxim_Laura_Lab8.Models;
+
+namespace Maxim_Laura_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Maxim_Laura_Lab8.Data.Maxim_Laura_Lab8Context _context;
+
+        public IndexModel(Maxim_Laura_Lab8.Data.Maxim_Laura_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
